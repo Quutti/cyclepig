4fbd0f8041b55443bd4e09eac6572ebfbda4d8ea
Jokke Liikkanen <quutticode@gmail.com>
2017-09-21 21:51:07 +0300

    Add index.ts barrel for navigation-bar component

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { AuthGuard } from "./auth-guard.service";
 import { UserService } from "./user.service";
 
-import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
+import { NavigationBarComponent } from "./navigation-bar";
 
 @NgModule({
     imports: [],
