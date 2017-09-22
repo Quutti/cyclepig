878d6ffdf5de356170a9bc82c5539ec7c9686cd8
Jokke Liikkanen <quutticode@gmail.com>
2017-09-22 22:14:10 +0300

    Remove widget as a term and use components as components under views

---
+++ client/src/app/home/add-ride/add-ride.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "hm-add-ride",
+    templateUrl: "./add-ride.component.html"
+})
+export class AddRideComponent {
+
+}
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared";
 
 import { HomeView } from "./home.view";
-import { AddRideWidget } from "./widgets/add-ride";
+import { AddRideComponent } from "./add-ride";
 
 @NgModule({
     imports: [SharedModule],
     exports: [HomeView],
     declarations: [
         HomeView,
-        AddRideWidget
+        AddRideComponent
     ]
 })
 export class HomeModule { }
