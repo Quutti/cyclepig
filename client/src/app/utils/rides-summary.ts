808cc1495cc37f1c1c23fb5ddac630fd389bb529
Jokke Liikkanen <quutticode@gmail.com>
2017-12-02 21:21:27 +0200

    First implementation of the module rides-summary model - Add also jsonDateToDate and dateToJsonDate methods to dateutils

---
+++ client/src/app/utils/rides-summary.ts
@@ -0,0 +1,105 @@
+
+import { Ride } from "../store/types";
+import * as dateUtils from "@shared/date-utils";
+
+export type RidesSummaryInterval = "daily" | "weekly" | "monthly" | "yearly" | "all";
+
+export interface RidesSummaryOptions {
+    interval: RidesSummaryInterval;
+    startDate: Date;
+    endDate: Date;
+    bikeId?: number;
+}
+
+export type RidesSummaryLabeler = (date: Date) => string;
+
+export type RidesSummaryModifier = (date: Date) => Date;
+
+export interface RidesSummaryItem {
+    label: string;
+    distance: number;
+    rides: number;
+}
+
+export const getRidesSummary = (rides: Ride[], options: RidesSummaryOptions): RidesSummaryItem[] => {
+
+    const res: RidesSummaryItem[] = [];
+
+    const labeler = getLabeler(options.interval);
+    const dateModifier = getDateModifier(options.interval);
+
+    const labelMap: string[] = [];
+
+    let currentDate = new Date(options.startDate.valueOf());
+    while (currentDate.valueOf() < options.endDate.valueOf()) {
+        const label = labeler(currentDate);
+        res.push({
+            label,
+            distance: 0,
+            rides: 0
+        });
+
+        labelMap.push(label);
+
+        // If interval is all break after first iteration
+        if (options.interval === "all") {
+            break;
+        }
+
+        currentDate = dateModifier(currentDate);
+    }
+
+    for (let ride of rides) {
+        if (options.bikeId && ride.bikeId !== options.bikeId) {
+            continue;
+        }
+
+        const label = labeler(dateUtils.jsonDateToDate(ride.date));
+        const labelIndex = labelMap.indexOf(label);
+
+        if (labelIndex > -1) {
+            res[labelIndex].distance += ride.distance;
+            res[labelIndex].rides += 1;
+        }
+    }
+
+    return res;
+}
+
+const getLabeler = (interval: RidesSummaryInterval): RidesSummaryLabeler => {
+    const prefix = (value: number): string => (value < 10) ? `0${value}` : `${value}`;
+
+    return (date: Date) => {
+        if (interval === "all") {
+            return "all";
+        } else if (interval === "yearly") {
+            return `${date.getFullYear()}`;
+        } else if (interval === "monthly") {
+            const m = prefix(date.getMonth() + 1);
+            return `${m}/${date.getFullYear()}`;
+        } else if (interval === "weekly") {
+            const w = dateUtils.getWeekNumber(date);
+            return `${w}-${date.getFullYear()}`;
+        } else {
+            const d = prefix(date.getDate());
+            const m = prefix(date.getMonth() + 1);
+            return `${date.getFullYear()}-${m}-${d}`;
+        }
+    }
+}
+
+const getDateModifier = (interval: RidesSummaryInterval): RidesSummaryModifier => {
+    return (date: Date) => {
+        const d = new Date(date.valueOf());
+        if (interval === "yearly") {
+            d.setFullYear(date.getFullYear() + 1);
+        } else if (interval === "monthly") {
+            d.setMonth(date.getMonth() + 1);
+        } else if (interval === "weekly") {
+            d.setDate(date.getDate() + 7);
+        } else {
+            d.setDate(date.getDate() + 1);
+        }
+        return d;
+    }
+}
--- shared/date-utils.ts
+++ shared/date-utils.ts
@@ -13,3 +13,34 @@ export const getMonthDayCount = (year: number, month: number): number => {
 
     return [31, (isLeapYear ? 29 : 28), 31, 30, 31, 31, 30, 31, 30, 31][month - 1];
 }
+
+/**
+ * @see http://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php/6117889#6117889
+ */
+export const getWeekNumber = (date: Date): number => {
+    const d = new Date(date.valueOf());
+    d.setHours(0, 0, 0, 0);
+    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+
+    const todayMs = d.valueOf();
+    const janFirstMs = new Date(d.getFullYear(), 0, 1).valueOf();
+
+    return Math.ceil(((((todayMs - janFirstMs) / 8.64e7) + 1) / 7));
+}
+
+
+export const jsonDateToDate = (jsonDate: string): Date => {
+    const parts = jsonDate.split("-");
+    const date = parseInt(parts[2], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[0], 10);
+    return new Date(year, month, date);
+}
+
+export const dateToJsonDate = (date: Date): string => {
+    const prefix = (val: number): string => (val < 10) ? `0${val}` : `${val}`;
+    const y = date.getFullYear();
+    const m = date.getMonth() + 1;
+    const d = date.getDate();
+    return `${y}-${prefix(m)}-${prefix(d)}`;
+}
